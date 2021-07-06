65d6e420039f753d0671cd5bf02699d319e2a2d0
wonder <xanwidtf@foxmail.com>
2021-07-06 22:32:15 +0800

    swagger

--- service/auths/src/main/java/com/wonder/auth/authApplication.java
+++ service/auths/src/main/java/com/wonder/auth/authApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.wonder")
 @MapperScan("com.wonder.auth.mapper")
+@ComponentScan("com.wonder")
 public class authApplication {
     public static void main(String[] args) {
         SpringApplication.run(authApplication.class,args);
--- service/auths/src/main/java/com/wonder/auth/controller/LoginController.java
+++ service/auths/src/main/java/com/wonder/auth/controller/LoginController.java
@@ -7,13 +7,14 @@ import com.wonder.common.result.Result;
 import com.wonder.common.result.ResultCodeEnum;
 import com.wonder.model.dto.SysUserDto;
 import com.wonder.model.entity.SysUser;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@Api(tags = "登录")
 @RestController
 @RequestMapping("auth")
 public class LoginController extends SuperRest {
@@ -21,7 +22,8 @@ public class LoginController extends SuperRest {
     @Autowired
     private ISysUserService sysUserService;
 
-    @RequestMapping("/login")
+    @ApiOperation("登录")
+    @PostMapping("/login")
     public Result login(
             @RequestBody SysUser sysUser
             ) {
